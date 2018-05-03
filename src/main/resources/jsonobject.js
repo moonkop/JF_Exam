1bf84f3eb5ba02da6b4909f56f86207ebaab75e0
moonkop <moonkop@qq.com>
2018-05-03 22:12:34 +0800

    working on ajax object

---
+++ src/main/resources/jsonobject.js
@@ -0,0 +1,13 @@
+var responseObj = {
+    code: 100,
+    msg: "success",
+    load:{
+        rows:[
+            {id:123,name:"李刚",school:"机蜂夏令营"}
+        ],
+        total:10
+    },
+    trace:{
+
+    }
+};
