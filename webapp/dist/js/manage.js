0ccb5e51cc2b7f09e10ceaf28808f0b20c024bef
moonkop <moonkop@qq.com>
2018-05-12 20:09:05 +0800

    working on question list # update git ignore

---
+++ webapp/dist/js/manage.js
@@ -0,0 +1,72 @@
+tableResponseHandler = function (res) {
+    return res.payload;
+}
+
+queryParams = function (params) {
+    var temp = {
+        pageNum: params.pageNumber,
+        pageSize: params.limit
+    };
+    return temp;
+}
+
+getFormData=function(formid)
+{
+    data={};
+    $.each($("#"+formid).serializeArray(), function (index, item) {
+        data[item.name] = item.value;
+    })
+    return data;
+
+}
+
+
+function OnResult(result, onsuccess, onfailure)
+{
+    if (result.code == 100)
+    {
+        if (onsuccess === undefined)
+        {
+            defaultOnSuccess(result);
+
+        } else if (onsuccess === 0)
+        {
+
+        } else
+        {
+            onsuccess(result);
+        }
+    } else
+    {
+        if (onfailure === undefined)
+        {
+            defaultOnFailure(result);
+        } else if (onfailure === 0)
+        {
+
+        } else
+        {
+            onfailure(result);
+        }
+    }
+}
+
+function defaultOnSuccess(result)
+{
+    if (result.message == "ok")
+    {
+        layer.msg('操作成功');
+    } else if (result.message === undefined)
+    {
+        layer.msg('操作结果未知');
+    } else
+    {
+        layer.msg('result.message');
+    }
+}
+
+function defaultOnFailure(res)
+{
+
+    layer.alert("操作失败" + res.code + ":" + res.message);
+}
