0ac080659a4d2493d1ab2103aa77ec1b52be79ec
moonkop <moonkop@qq.com>
2018-06-14 17:33:13 +0800

    backup mysql

--- sqldump/jf_exam_paper.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE jf_exam.paper
-(
-    id char(32) PRIMARY KEY NOT NULL,
-    title varchar(255),
-    comment varchar(1000),
-    create_teacher_id char(32) NOT NULL,
-    subject_id tinyint(4) NOT NULL,
-    questionContain text
-);
--- sqldump/jf_exam_tresourcetype.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE jf_exam.tresourcetype
-(
-    ID varchar(36) PRIMARY KEY NOT NULL,
-    NAME varchar(100) NOT NULL
-);
-INSERT INTO jf_exam.tresourcetype (ID, NAME) VALUES ('0', '菜单');
-INSERT INTO jf_exam.tresourcetype (ID, NAME) VALUES ('1', '功能');
